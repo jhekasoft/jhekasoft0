dd5beb6e1c0eb7ef98e87648a500467bc2e0150f
jhekasoft <jheka@mail.ru>
2013-08-03 22:05:06 +0300

    Display exceptions moved to local.php config

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -129,8 +129,8 @@ return array(
         ),
     ),
     'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
+//        'display_not_found_reason' => true,
+//        'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
