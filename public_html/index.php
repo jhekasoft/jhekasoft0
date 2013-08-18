de0a5a63be4d933d915a45e05479f1ec22079ba9
jhekasoft <jheka@mail.ru>
2013-08-19 01:23:50 +0300

    Style-changes and display errors on dev
    Transitions on buttons

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Display all errors when APPLICATION_ENV is development.
+ */
+if ($_SERVER['APPLICATION_ENV'] == 'development') {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+}
+
 // Set timezone
 if (function_exists('date_default_timezone_set')) {
 	date_default_timezone_set('Europe/Kiev');
